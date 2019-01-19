032239418e6ad518c4c7412e67732ec82f8479c5
Fabian Diaz <16745697+fdiaz79@users.noreply.github.com>
2019-01-19 11:29:37 -0500

    javascript pushing data into firebase

---
+++ assets/javascript/script.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyADJwMlDHnbVWAsalmvYlp4VWR0YR-rBnI",
+        authDomain: "time-sheet-f9025.firebaseapp.com",
+        databaseURL: "https://time-sheet-f9025.firebaseio.com",
+        projectId: "time-sheet-f9025",
+        storageBucket: "time-sheet-f9025.appspot.com",
+        messagingSenderId: "787348602659"
+    };
+    firebase.initializeApp(config);
+
+    var database = firebase.database();
+
+    $("#submit").on("click", function(event){
+        event.preventDefault();
+
+        var employeeName = $("#employee-name").val().trim();
+        var employeeRole = $("#employee-role").val().trim();
+        var employeeDate = $("#employee-date").val().trim();
+        var employeeRate = $("#employee-rate").val().trim();
+
+        var newEmp = {
+            name: employeeName,
+            role: employeeRole,
+            date: employeeDate,
+            rate: employeeRate
+        };
+
+        database.ref().push(newEmp);
+
+        console.log(newEmp.name);
+        console.log(newEmp.role);
+        console.log(newEmp.date);
+        console.log(newEmp.rate);
+
+
+    });
+
+});
